ce67f462b0c5a28ede29ad9799688b4bee6b7337
tanjim750 <mdtanjim750@gmail.com>
2023-12-29 21:03:37 +0600

    wip

--- runserver.bat
+++ runserver.bat
@@ -8,6 +8,9 @@ set "SCRIPT_DIR=%~dp0"
 rem Navigate to the project directory
 cd /d "%SCRIPT_DIR%"
 
+rem Pulling changes from GitHub...
+git pull https://github.com/tanjim750/wafid_local
+
 rem Activate the virtual environment
 call venv\Scripts\activate
 
