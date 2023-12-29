44738fc399ac7997f290f7dc5a8e3acf1097ab66
tanjim750 <mdtanjim750@gmail.com>
2023-12-29 22:32:38 +0600

    wip

--- runserver.bat
+++ runserver.bat
@@ -11,14 +11,8 @@ cd /d "%SCRIPT_DIR%"
 rem Pulling changes from GitHub...
 git pull https://github.com/tanjim750/wafid_local
 
-rem Activate the virtual environment
-call venv\Scripts\activate
-
 rem Install dependencies
 pip install -r requirements.txt
-
-rem Run migrations
-python manage.py migrate
 
 rem Start Django development server
 python manage.py runserver
@@ -29,7 +23,5 @@ timeout /t 5
 rem Open the default browser to the Django development server address
 start "" http://127.0.0.1:8000/
 
-rem Deactivate the virtual environment
-deactivate
 
 endlocal
