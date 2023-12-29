11856a2e48e3b7d40fdedc63a17cb8042266afbb
tanjim750 <mdtanjim750@gmail.com>
2023-12-29 21:36:12 +0600

    wip

--- initial.bat
+++ initial.bat
@@ -1,12 +1,3 @@
-@echo off
-set PYTHON_VERSION=3.8.5  # Change this to the version you downloaded
-set GIT_VERSION=2.35.1   # Change this to the version you want
-
-:: Install Python for all users
-msiexec /i python-%PYTHON_VERSION%-amd64.exe /quiet TARGETDIR=C:\Python38 /ADDLOCAL=ALL /PYTHONALLUSERS=1 /PrependPath=1
-
-:: Install Git for all users
-msiexec /i Git-%GIT_VERSION%-64-bit.exe /quiet /COMPONENTS="icons,ext\reg\shellhere,assoc,assoc_sh" /DIR="C:\Git" /ADDLOCAL=FeatureSelect /INSTALLDIR="C:\Git"
-
-pip install virtualenv
+Invoke-WebRequest -UseBasicParsing -Uri 'https://www.python.org/ftp/python/3.11.0/python-3.11.0-amd64.exe' -OutFile 'c:/veera/python-3.11.0-amd64.exe'
+.\python-3.11.0-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
 
