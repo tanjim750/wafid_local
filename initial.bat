d5d86df4e49f9945ebca1bc036976aee5d8b7465
tanjim750 <mdtanjim750@gmail.com>
2023-12-29 21:47:54 +0600

    wip

--- initial.bat
+++ initial.bat
@@ -1,3 +1,12 @@
-Invoke-WebRequest -UseBasicParsing -Uri 'https://www.python.org/ftp/python/3.11.0/python-3.11.0-amd64.exe' -OutFile 'c:/veera/python-3.11.0-amd64.exe'
-.\python-3.11.0-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+@echo off
 
+:: Download Python installer
+powershell.exe -Command "Invoke-WebRequest -UseBasicParsing -Uri 'https://www.python.org/ftp/python/3.11.0/python-3.11.0-amd64.exe' -OutFile 'python-3.11.0-amd64.exe'"
+
+:: Install Python
+powershell.exe -Command "Start-Process -Wait -FilePath .\python-3.11.0-amd64.exe -ArgumentList '/quiet', 'InstallAllUsers=1', 'PrependPath=1', 'Include_test=0'"
+
+:: Remove the installer after installation (optional)
+del python-3.11.0-amd64.exe
+
+enter
