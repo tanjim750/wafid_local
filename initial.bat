26ecc96f40f5be8423f0252919f51782bc0b5f6c
tanjim750 <mdtanjim750@gmail.com>
2023-12-29 22:02:59 +0600

    wip

--- initial.bat
+++ initial.bat
@@ -1,4 +1,3 @@
-@echo off
 
 :: Download Python installer
 powershell.exe -Command "Invoke-WebRequest -UseBasicParsing -Uri 'https://www.python.org/ftp/python/3.11.0/python-3.11.0-amd64.exe' -OutFile 'python-3.11.0-amd64.exe'"
@@ -6,7 +5,8 @@ powershell.exe -Command "Invoke-WebRequest -UseBasicParsing -Uri 'https://www.py
 :: Install Python
 powershell.exe -Command "Start-Process -Wait -FilePath .\python-3.11.0-amd64.exe -ArgumentList '/quiet', 'InstallAllUsers=1', 'PrependPath=1', 'Include_test=0'"
 
-:: Remove the installer after installation (optional)
-del python-3.11.0-amd64.exe
+:: Download Git installer
+powershell.exe -Command "Invoke-WebRequest -UseBasicParsing -Uri 'https://github.com/git-for-windows/git/releases/download/v2.36.0.windows.1/Git-2.36.0-64-bit.exe' -OutFile 'Git-2.36.0-64-bit.exe'"
 
-enter
+:: Install Git
+powershell.exe -Command "Start-Process -Wait -FilePath .\Git-2.36.0-64-bit.exe -ArgumentList '/SILENT /NORESTART /CLOSEAPPLICATIONS /COMPONENTS=icons,ext\reg\shellhere,assoc,assoc_sh'"
