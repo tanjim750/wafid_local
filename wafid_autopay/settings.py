622ef98421aa2fb824a95d2c04f4c8237dbc4cf0
tanjim750 <mdtanjim750@gmail.com>
2024-01-05 18:33:27 +0600

    Done

--- wafid_autopay/settings.py
+++ wafid_autopay/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'wafid_autopay.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'USER': 'wafid',
+        'PASSWORD': 'Nur@24travels',
+        'HOST': '0.0.0.0',
+        'PORT' : '3306',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # DATABASES = {
 #    'default': {
